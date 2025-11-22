60cca53ad0b4850befefa703a26ebcea271b6525
faarouqsocials@gmail.com <faarouq@Faarouq.>
2025-11-22 18:22:51 +0000

    - Added  to collect key system diagnostics:   • Timestamp, hostname, disk, memory, swap, CPU, processes, users   • Failed login attempts with regex-based grep logic - Implemented grouped log redirection and error logging - Fixed earlier issues:   • Corrected timestamp formatting   • Fixed swap calculation   • Improved output headings and added proper units   • Enabled process sorting via descending CPU/MEM   • Added fallback logic for missing auth log entries   • Created log directory under /home/faarouq if not present - Validated script execution and outputs manually

--- health_report.sh
+++ health_report.sh
@@ -4,27 +4,45 @@
 #Descr: An automated health-report tool that collects and logs real-time system diagnostics
 
 
-DESTINATION="$HOME/logs"
-TIMESTAMP="$(date '+%F +%T')"
+DESTINATION="$HOME/logs/health_report.log"
+TIMESTAMP="$(date '+%F %T')"
+ERRORLOG="$HOME/logs/health_report.err"
         # Memory Stats
-TOTALMEMORY=$(awk '/MemTotal/ {print $2}')
-FREEMEMORY=$(awk '/MemFree/ {print $2}')
-TOTALMEMORY=$(awk '/MemTotal/ {print $2}')
+TOTALMEMORY=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+FREEMEMORY=$(awk '/MemFree/ {print $2}' /proc/meminfo)
+TOTALMEMORY=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
 USEDMEMORY=$((TOTALMEMORY - FREEMEMORY))
+	#Swap Stats
+TOTALSWAP=$(awk '/SwapTotal/ {print $2}' /proc/meminfo)
+FREESWAP=$(awk '/SwapFree/ {print $2}' /proc/meminfo)
+USEDSWAP=$((TOTALSWAP - FREESWAP))
 
 {
 	printf '%s\n'"============================"
-	printf '\n Hostname: \n'
+	printf '\n Hostname: '
 	hostname
-	printf '\n Timestamp: %s\n' "$TIMESTAMP"
-	printf '\n Current Working Directory : \n'
+	printf '\n Timestamp: %s' "$TIMESTAMP"
+	printf '\n Current Working Directory : '
 	pwd
 	printf '\n Disk Usage: \n'
-	df
-	printf '\n\n Memory Stats\n'
+	df -h
+	printf '\n\n Memory Stats (kB)\n'
 	printf 'Total Memory: %s\n' "$TOTALMEMORY"
 	printf 'Used Memory: %s\n' "$USEDMEMORY"
 	printf 'Free Memory: %s\n' "$FREEMEMORY"
-
+	printf '\n\n Swap Stats (kB)\n'
+	printf 'Total Swap: %s\n' "$TOTALSWAP"
+        printf 'Used Swap: %s\n' "$USEDSWAP"
+        printf 'Free Swap: %s\n' "$FREESWAP"
+	printf '\n\n Top 5 Memory-consuming processes \n'
+	ps -eo pid,comm,%mem --sort=-%mem | head -n 6
+        printf '\n\n Top 5 CPU-consuming processes \n'
+	ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+	printf '\n\n Current Logged in Users \n'
+	who
+	printf '\n \n Failed Login Attempts\n'
+	grep -Ei "failure|error|invalid" /var/log/auth.log 2>/dev/null || echo "No failed login attempt found"
 	
-}
+
+
+} >>$DESTINATION 2>>"$ERRORLOG"
