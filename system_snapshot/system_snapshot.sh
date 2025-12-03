35051f36b7db2f44ac6ae8a82f927f90d56bc613
faarouqsocials@gmail.com <faarouq@Faarouq.>
2025-12-03 18:24:53 +0000

    Added system_snapshot.sh for basic system reporting

--- system_snapshot/system_snapshot.sh
+++ system_snapshot/system_snapshot.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #AUTHOR: FAAROUQ ASAJU
 #DATE: 03-12-2025
 #DESCRIPTION: Collects simple system information
@@ -7,4 +8,25 @@
 
 HOSTNAME=$(hostname)
 OSNAME=$(uname)
-KERNELVERSION=$(awk)
+KERNELVERSION=$(uname -r)
+LOGGEDIN=$(who | awk '{print $1}' | sort -u | paste -sd, -)
+DATE=$(date '+%F %T')
+LOG_FILE=$HOME/logs/system_snapshot.log
+
+
+{
+
+	printf '%s\n'"===================="
+	printf '\n TIMESTAMP: %s\n' "$DATE"	
+	printf '\n Hostname: %s\n' "$HOSTNAME"
+	printf '\n OS: %s\n' "$OSNAME"
+	printf '\n Kernel Version: %s\n' "$KERNELVERSION"
+	printf '\n Logged-in Users: %s\n' "$LOGGEDIN"
+	printf '\n Disk Usage Summary: \n'
+	df -h
+	printf '\n Memory Usage Summary: \n'
+	free -h
+	printf '%s\n'"==================== \n\n\n\n"
+	
+}>> $LOG_FILE
+
