e6c5fc80d31cf6a536edc8aa18d22dba7e29d934
Faarouq Asaju <faarouqsocials@gmail.com>
2025-12-24 15:43:20 +0000

    feat: add function to list active systemd services

--- service_exposure_audit/service_exposure_edit.sh
+++ service_exposure_audit/service_exposure_edit.sh
@@ -3,3 +3,18 @@
 #Date: 24-12-2025
 #Descr: lists running services and listening ports to identify potential system exposure.
 
+
+list_active_services(){
+
+	printf 'Active Services: \n\n'
+
+	active_services=$(systemctl list-units --type=service --state=running --no-legend  | awk '{print "\t- " $1}')
+	if [ -z "$active_services" ]; then
+		printf 'None detected\n'
+		return 0
+	else
+		echo "$active_services"
+	fi
+}
+
+list_active_services
